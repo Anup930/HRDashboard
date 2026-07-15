7222603cb98d50079b1a1006f42657cad8da9eaa
Anup930 <Anup.gretex@gmail.com>
2026-07-15 11:23:19 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// === GRETEX WFH TRACKER - CONFIG ===
-// Paste your Apps Script Web App URL below after deploying Code.gs
-const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxZDzoonHO8e0kYin0j7tos2jEED8tRV9OgvI3BoPwd_L2lqyf3PsdkYcJM4WpRKiDsYg/exec',
-  APP_NAME: 'Gretex WFH Tracker',
-  ORG_NAME: 'Gretex Group'
-};
